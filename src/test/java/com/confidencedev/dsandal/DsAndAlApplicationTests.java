88d5d7a24f6c88487d61a864c4dd8ee69df921ee
ConfidenceDev <confidostic3@gmail.com>
2023-03-18 15:12:39 +0100

    Migrated
    Moved Project from Eclipse to IntelliJ

---
+++ src/test/java/com/confidencedev/dsandal/DsAndAlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confidencedev.dsandal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsAndAlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
