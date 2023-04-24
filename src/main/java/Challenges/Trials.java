345f3d68a19936996a2e66f23995dc4a2834c4bb
ConfidenceDev <confidostic3@gmail.com>
2023-04-24 21:32:25 +0100

    Run Test

--- src/main/java/Challenges/Trials.java
+++ src/main/java/Challenges/Trials.java
@@ -1,8 +1,13 @@
 package Challenges;
 
+import java.util.*;
+
 public class Trials {
 
     public static void main(String[] args) {
-        
+
+        List<String> cases = new ArrayList<>();
+
+        Set<String> strings = new TreeSet<>(cases);
     }
 }
